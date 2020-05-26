5e6774b640200b99cd6a2c640c2c89e6ef4e1b5b
Florian GRENIER <flogrenier.pro@gmail.com>
2020-05-26 22:20:37 +0200

    First style and ts architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { PlayerComponent } from './player/player.component';
+import { GameComponent } from './game/game.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    PlayerComponent,
+    GameComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule,
